f3325b828e9218197847212ed37642eb9910a8e9
alumno <alumno@sldid04-2.local>
2019-03-26 17:22:58 -0600

    le agregue la respuesta a la kata de friend or foe

--- Prueba.py
+++ Prueba.py
@@ -1,1 +1,17 @@
-h iqegaoyg nearigdig v<bñk
+lista = ["Xime", "Nube","Abi", "Emi", "Isma"]
+lista_buena = []
+def FRIENDS(x):
+	for nombre in lista:
+		if len(nombre) == 4:
+			lista_buena.append(nombre)
+
+	return lista_buena
+
+print FRIENDS(lista)
+
+def friend(x):
+    lista = []
+    for nombre in x:
+        if len(nombre) == 4:
+            lista.append(nombre)
+    return lista
